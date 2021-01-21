9757fdf6c81cb0464d0a35475713685e2c7d046d
Dan Ross <daniellross@gmail.com>
2021-01-21 14:02:28 -0600

    added best in show function. called twice on main and binded to variables.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 import { calculateAgeInDogYears } from './DogYears.js'
+import { favoriteBreed } from './BestInShow.js'
+
 console.log("Welcome to the main module")
 
 const dogAge = calculateAgeInDogYears(22)
 console.log(`Age in dog years: ${dogAge}`)
+
+const cat = favoriteBreed('meow');
+const danFavorite = favoriteBreed('labradoodle');
+console.log(cat)
+console.log(danFavorite)
