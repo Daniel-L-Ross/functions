07b93a43da62f8c08b43abc728f538de2284c4a2
Dan Ross <daniellross@gmail.com>
2021-01-21 18:34:10 -0600

    refactored the chore function to take 3 chores

--- scripts/Chores.js
+++ scripts/Chores.js
@@ -1,21 +1,31 @@
 // chores functions
-const washDishes = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} washed the dishes.`
+
+export const danobj = {
+    firstName: 'Dan',
+    lastName: 'Ross',
 }
-const emptyRecycling = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} emptied the recycling bin.`
+
+export const washDishes = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} washed the dishes`
 }
-const emptyTrash = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} emptied the trash can.`
+export const emptyRecycling = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} emptied the recycling bin`
 }
-const getMail = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} got the mail.`
+export const emptyTrash = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} emptied the trash can`
 }
-const payRent = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} paid the rent.`
+export const getMail = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} got the mail`
 }
-const sweepFloor = (personObj) => {
-    return `${personObj.firstName} ${personObj.lastName} swept the floor.`
+export const payRent = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} paid the rent`
+}
+export const sweepFloor = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} swept the floor`
+}
+
+export const dayPlanner = (firstChore, secondChore, thirdChore, person, day) => {
+    return `On ${day}, ${firstChore(person)}, and ${secondChore(person)}, and ${thirdChore(person)}.`
 }
 
 
