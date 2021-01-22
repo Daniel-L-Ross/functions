c75cf1923396cb5549bab8c8b6333c296111fdb8
Dan Ross <daniellross@gmail.com>
2021-01-21 18:16:43 -0600

    chores functions

--- scripts/Chores.js
+++ scripts/Chores.js
@@ -1,3 +1,22 @@
+// chores functions
+const washDishes = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} washed the dishes.`
+}
+const emptyRecycling = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} emptied the recycling bin.`
+}
+const emptyTrash = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} emptied the trash can.`
+}
+const getMail = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} got the mail.`
+}
+const payRent = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} paid the rent.`
+}
+const sweepFloor = (personObj) => {
+    return `${personObj.firstName} ${personObj.lastName} swept the floor.`
+}
 
 
 // mathmatic examples of higher level functons
